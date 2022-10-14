0eff99b25d315cded12945289c0504d6d30bad71
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 20:25:36 -0300

    Contagem Foguete

---
+++ foguete.py
@@ -0,0 +1,7 @@
+# 1 - Faça um programa para escrever a contagem regressiva do lançamento de um foguete.
+# O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela. Dê o nome do seu programa de foguete.py.
+c = 10
+while c >= 0:
+    print(c)
+    c = c - 1
+print("Fogo!")
